a5d64ac5bacc01732123a3c633430cb6cb055fdf
ribahdev <joao.ribeiro17@outlook.com>
2024-06-29 14:00:22 -0300

    Adicionando carrossel

--- EtecShop/obj/Debug/net8.0/EtecShop.AssemblyInfo.cs
+++ EtecShop/obj/Debug/net8.0/EtecShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EtecShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7dac2a8e13137611263595701a300621504354")]
 [assembly: System.Reflection.AssemblyProductAttribute("EtecShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EtecShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
